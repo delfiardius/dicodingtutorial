dafb5eb3e42177f5365e5aa465806c11fa0db9fe
delfiardius <ardigulo09@gmail.com>
2025-06-16 13:31:32 +0700

    menambahkan materi simulasi asynchronous process

---
+++ javascript/asynchronous_process/main.mjs
@@ -0,0 +1,7 @@
+import { makeCoffee } from "./coffee.mjs";
+
+console.log('Saya memesan kopi di kafe.');
+
+makeCoffee();
+console.log('Pramusaji memberikan kopi pesanan.');
+console.log('Saya mendapatkan kopi dan menghabiskannya');
