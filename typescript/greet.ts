b477379e1c27705195f9730f17d202b59ab9a52f
delfiardius <ardigulo09@gmail.com>
2025-06-19 12:36:06 +0700

    menginstal bun

--- typescript/greet.ts
+++ typescript/greet.ts
@@ -1,7 +1,7 @@
 const namaKu: string = 'TypeScript';
 
 function salam(name: string) {
-    console.log(`Hello, ${name}. My name is ${myName}`);
+    console.log(`Hello, ${name}. My name is ${namaKu}`);
 }
 
 salam('JavaScript');
