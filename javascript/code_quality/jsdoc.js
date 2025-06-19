b3cc542434812c633424d418b0514489f4f6ac0e
delfiardius <ardigulo09@gmail.com>
2025-06-19 11:00:09 +0700

    menambahkan materi jsdoc

---
+++ javascript/code_quality/jsdoc.js
@@ -0,0 +1,12 @@
+
+/**
+ * Get add operations of two numbers
+ * 
+ * @param {number} numA - The first numberic operand
+ * @param {number} numB - The second numberic operand
+ * @returns {number} Sum of numA and numB
+ */
+
+function add(numA, numB) {
+    return numA + numB;
+}
