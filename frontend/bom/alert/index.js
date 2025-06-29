f3c8c7ed2170cf16f97cfa919da1ae58e3ac47ca
delfiardius <ardigulo09@gmail.com>
2025-06-29 19:05:43 +0700

    menambahkan materi bom, method alert()

---
+++ frontend/bom/alert/index.js
@@ -0,0 +1,11 @@
+/**
+ * Method alert() merupakan salah satu method BOM yang menerima
+ * satu parameter opsional berupa data string yang akan 
+ * ditampilkan melalui sebuah pop-up browser. Contohnya, jika
+ * kita ingin menampilkan sebuah pesan berisi string "Halo, user!"
+ * menggunakan console browser, caranya sebagai berikut:
+ */
+
+
+let message = 'Hallo, user!'
+alert(message);
