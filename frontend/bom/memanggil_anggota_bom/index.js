b5392442dc2f0389f83d0564e803889e07952224
delfiardius <ardigulo09@gmail.com>
2025-07-03 11:22:39 +0700

    menambahkan materi cara memanggil anggota bom

---
+++ frontend/bom/memanggil_anggota_bom/index.js
@@ -0,0 +1,15 @@
+/**
+ * Catatan:
+ * 
+ * Hati-hati jika kita mendefinisikan sebuah method dengan nama yang
+ * sama dengan method milik window. Jika sama, kemungkinan besar 
+ * method window tidak berfungsi.
+ */
+
+// function alert(nama) {
+//     console.log('Hati-hati,' + nama);
+// }
+
+// alert('awwww');
+
+window.alert('Windows Alert');
