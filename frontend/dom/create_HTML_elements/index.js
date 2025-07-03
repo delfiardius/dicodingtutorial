113861fbbd59ed477585fa91dc2a036f1fe1f551
delfiardius <ardigulo09@gmail.com>
2025-07-03 12:44:33 +0700

    menambahkan materi cara membuat element HTML melalui JS

---
+++ frontend/dom/create_HTML_elements/index.js
@@ -0,0 +1,23 @@
+/**
+ * 1. Membuat element p HTML
+ * 
+ * const newElement = document.createElement("p");
+ * 
+ * 2. Menambahkan teks pada element p
+ * 
+ * newElement.innerText = 'Selamat datang di HTML Kosong ini';
+ * 
+ * 3. Menjadikan Tulisan 'Selamat datang' cetak tebal
+ * 
+ * newElement.innerText = '<b>Selamat datang</b> di HTML Kosong ini';
+ * 
+ * 4. Membuat element img HTML
+ * 
+ * const newElementImg = document.createElement("img");
+ * 
+ * 5. Menambahkan atribut src pada img
+ * 
+ * newElementImg.setAttribute('src', 'https://unsplash.com/photos/a-man-sitting-at-a-table-reading-a-book-LsPf45g6S5E');
+ * 
+ * function 'setAttribute()' menerima dua parameter, yaitu nama atribut yang ingin ditambahkan dan nilai yang ingin diberikan dalam atribut tersebut.
+ */
