be28cebf14dfe2a19a2302b814a9f4e40e17609e
delfiardius <ardigulo09@gmail.com>
2025-07-03 11:06:52 +0700

    menambahkan materi objec console

---
+++ frontend/bom/console/index.js
@@ -0,0 +1,24 @@
+/**
+ * Objeck console merupakan peralatan yang wajib diketahui oleh semua
+ * Front-End Web Developer. Mengapa demikian? Hal ini karena objek 
+ * console memberikan kita akses ke fitur debugging alias peralatan-
+ * peralatan yang bisa membantu menghilangkan bug yang bersembunyi
+ * di dalam kode JavaScript.
+ * 
+ * Melalui objek console ini, kita dapat membuat kode JavaScript
+ * menampilkan pesan-pesan khusus berdasarkan konteks tertentu pada 
+ * console browser.
+ */
+
+
+/**Method dari objek console yang sering digunakan:
+ * 1. log() -> Menampilkan pesan secara umum ke console browser.
+ * 2. info() -> Menampilkan pesan yang berisi informasi penting ke console browser.
+ * 3. warn() -> Menampilkan pesan yang berisi informasi dalam bentuk peringatan ke console browser.
+ * 4. error() -> Menampilkan pesan yang berisi informasi dalam bentuk erro ke console browser.
+ */
+
+// console.log('Ini adalah console log');
+// console.info('Ini adalah console info');
+// console.warn('Ini adalah console warn');
+console.error('Ini adalah console error');
