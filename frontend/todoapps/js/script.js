bcfe1640b3f5f88d24a395eb5bfd9b07a7952291
delfiardius <ardigulo09@gmail.com>
2025-09-06 09:09:00 +0700

    menambahkan listener DOMContentLoaded

---
+++ frontend/todoapps/js/script.js
@@ -0,0 +1,48 @@
+/**Berkas ini digunakan untuk menyimpan beberapa kode logika yang bertujuan untuk
+ * memanipulasi/mengontrol element yang ada di HTML.
+ * 
+ * Pada TodoApps ini, kita melakukan beberapa hal berikut dengan JavaScript:
+ * @argument menambahkan TODO baru
+ * @argument memindahkan TODO
+ * @argument menghapus TODO
+ */
+
+const todos = [];
+const RENDER_EVENT = 'render-todo';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const submitForm = document.getElementById('form');
+    submitForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        addTodo();
+    });
+})
+
+function addTodo() {
+    const textTodo = document.getElementById('title').value;
+    const timestamp = document.getElementById('date').value;
+
+    const generatedID = generateId();
+    const todoObject = generateTodoObject(generatedID, textTodo, timestamp, false);
+    todos.push(todoObject);
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function generateId() {
+    return +new Date();
+}
+
+function generateTodoObject(id, task, timestamp, isCompleted) {
+    return {
+        id,
+        task,
+        timestamp,
+        isCompleted
+    }
+}
+
+document.addEventListener(RENDER_EVENT, function() {
+    console.log(todos);
+});
+
